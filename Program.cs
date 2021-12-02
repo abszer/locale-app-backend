1403bb63cdc14edafd86b419f91fb0a61e5b4a45
Robert White <robert.white3120@gmail.com>
2021-12-02 15:23:47 -0600

    added code for handling file uploads to server

--- Program.cs
+++ Program.cs
@@ -23,6 +23,9 @@ builder.Services.AddCors( opt => {
 });
 var app = builder.Build();
 
+
+
+
 app.UseCors( builder => {
     builder.AllowAnyHeader()
             .AllowAnyMethod()
@@ -43,6 +46,43 @@ app.MapGet("/api/posts/{PostId}", async (int PostId, PostDb db) =>
             is Post post
             ? Results.Ok(post)
             : Results.NotFound());
+
+
+// FILE STORAGE STUFF -- https://medium.com/@francesca.paterinaldi/building-a-simple-file-storage-server-with-net-core-ad608ca3dc05
+app.MapPost("/upload", async (HttpRequest req) => 
+{
+    if(!req.HasFormContentType)
+    {
+        return Results.BadRequest();
+    }
+
+    var form = await req.ReadFormAsync();
+    var file = form.Files["file"];
+
+    if(file is null)
+    {
+        return Results.BadRequest();
+    }
+
+    //differentiate home paths based on os
+    string homePath = (Environment.OSVersion.Platform == PlatformID.Unix)
+    ? Environment.GetEnvironmentVariable("HOME") 
+    : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+
+    string uploadsPath = Path.Combine(homePath, "uploads");
+    string newFileName = DateTime.Now.Ticks + "_" + Guid.NewGuid().ToString();
+
+    Directory.CreateDirectory(uploadsPath);
+    var filePath = Path.Combine(uploadsPath, newFileName);
+
+    await using var fileStream = File.OpenWrite(filePath);
+    await using var uploadStream = file.OpenReadStream();
+    await uploadStream.CopyToAsync(fileStream);
+
+    return Results.Text("Success!");
+
+
+}).Accepts<IFormFile>("multipart/form-data");
 
 // POST
 app.MapPost("/api/posts", async (Post post, PostDb db) => 
